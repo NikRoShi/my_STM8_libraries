a4b7fb64e9c266cd17c2e377c1fa9245e10df3f0
NikRoShi <nikolays2308@gmail.com>
2026-06-19 15:46:19 -0700

    исправлена ошибка с побитовыми сдвинами в init uart

--- stm8_UART.c
+++ stm8_UART.c
@@ -9,8 +9,8 @@ void init_UART(uint16_t baudrate)
 	
 	uartdiv = F_CPU / baudrate;
 	
-	UART1_BRR2 = (uartdiv & 0x000F) | ((uartdiv >> 2) & 0x00F0);
-	UART1_BRR1 = (uartdiv >> 1) & 0x00FF;
+	UART1_BRR2 = (uartdiv & 0x000F) | ((uartdiv >> 8) & 0x00F0);
+	UART1_BRR1 = (uartdiv >> 4) & 0x00FF;
 	
 	UART1_CR2 |= UART1_CR2_TEN;
 }
