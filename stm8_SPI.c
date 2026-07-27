0c7b85ff95bd829f88145a0abc23e454cc7ce07d
NikRoShi <nikolays2308@gmail.com>
2026-07-27 13:33:56 -0700

    spi.c добавлен

---
+++ stm8_SPI.c
@@ -0,0 +1,6 @@
+#include "stm8_SPI.h"
+
+void init_SPI(uint8_t mode, uint8_t div, uint8_t firstBit, uint8_t masterSlave)
+{
+	
+}
--- stm8_SPI.h
+++ stm8_SPI.h
@@ -4,26 +4,26 @@
 #include <stdint.h>
 #include "stm8_REG.h"
 
-#define MODE0 0
-#define MODE1 1
-#define MODE2 2
-#define MODE3 3
+#define SPI_MODE0 0
+#define SPI_MODE1 1
+#define SPI_MODE2 2
+#define SPI_MODE3 3
 
-#define DIV2 (0 << 3)
-#define DIV4 (1 << 3)
-#define DIV8 (2 << 3)
-#define DIV16 (3 << 3)
-#define DIV32 (4 << 3)
-#define DIV64 (5 << 3)
-#define DIV128 (6 << 3)
-#define DIV256 (7 << 3)
+#define SPI_DIV2 (0 << 3)
+#define SPI_DIV4 (1 << 3)
+#define SPI_DIV8 (2 << 3)
+#define SPI_DIV16 (3 << 3)
+#define SPI_DIV32 (4 << 3)
+#define SPI_DIV64 (5 << 3)
+#define SPI_DIV128 (6 << 3)
+#define SPI_DIV256 (7 << 3)
 
-#define MSB (0 << 7)
-#define LSB (1 << 7)
+#define SPI_MSB (0 << 7)
+#define SPI_LSB (1 << 7)
 
-#define MST (1 << 2)
-#define SLV (0 << 2)
+#define SPI_MST (1 << 2)
+#define SPI_SLV (0 << 2)
 
-void init_SPI(uint8_t mode, uint8_t div, uint8_t lsbfirst, uint8_t masterSlave);
+void init_SPI(uint8_t mode, uint8_t div, uint8_t firstBit, uint8_t masterSlave);
 
 #endif
